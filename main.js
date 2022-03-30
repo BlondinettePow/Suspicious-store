a0692bc63189cfd35251d6f6b574761d12472542
BlondinettePow <84834751+BlondinettePow@users.noreply.github.com>
2022-03-30 21:31:21 +0200

    d

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("running")
+let carts = document.querySelectorAll('.add-cart');
+let products = [
+    {
+        name: 'gold monkey',
+        tag: 'gmonkey',
+        price: 150,
+        inCart: 0,
+    },
+    {
+        name: 'human one',
+        tag: 'humanone',
+        price: 69,
+        inCart: 0,
+    }
+]
+
+for (let i=0; i< carts.length; i++){
+    carts[i].addEventListener('click', () => {
+        cartNumber();
+    })
+}
+
+function cartNumber(){
+    let productNumber = localStorage.getItem('cartNumber');
+    console.log(productNumber)
+    productNumber = parseInt(productNumber)
+    localStorage.setItem('cartNumber', 1);
+}
