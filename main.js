87dab0ef8ae9db0aee270d0ff8e8888512995e5e
BlondinettePow <84834751+BlondinettePow@users.noreply.github.com>
2022-04-01 21:55:47 +0200

    lol

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ let products = [
 for (let i=0; i < carts.length; i++){
     carts[i].addEventListener('click', () => {
         cartNumber(products[i]);
-        totalCost(products[i])
+        totalCost(products[i]);
     })
 }
 function onLoadCartNumber(){
